f12309064216ddc5374332260cea54a1c4189a29
albert <albert200000@gmail.com>
2022-07-28 15:49:57 +0300

    added typescript

--- main.js
+++ main.js
@@ -6,13 +6,13 @@ File:           main.js
 
 Description:    A plugin to extend code and markup language support in Adobe
                 Brackets using the native CodeMirror language modes.
-                
+
                 This plugin currently supports syntax highlighting and comment
                 hotkeys for Batch, Brainfuck, COBOL, Common Lisp, Cython, D,
-                Django, Fortran, HTTP, IDL, Julia, LaTeX, LiveScript,
+                Fortran, HTTP, IDL, Julia, LaTeX, LiveScript,
                 Mathematica, Pascal, PowerShell, R,
                 reStructuredText (RST/reST), Solidity, sTeX, and Swift.
-                
+
 Source:         github.com/ErickShepherd/more-languages-for-adobe-brackets
 
 Author:         Erick Edward Shepherd
@@ -27,28 +27,28 @@ Copyright (C) 2018 of Erick Edward Shepherd - All Rights Reserved.
 
 This file is part of "More Languages for Adobe Brackets".
 
-"More Languages for Adobe Brackets" is free software: you can redistribute it 
-and/or modify it under the terms of the GNU Affero General Public License as 
-published by the Free Software Foundation, either version 3 of the License, or 
+"More Languages for Adobe Brackets" is free software: you can redistribute it
+and/or modify it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
-"More Languages for Adobe Brackets" is distributed in the hope that it will be 
-useful, but WITHOUT ANY WARRANTY; without even the implied warranty of 
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General 
+"More Languages for Adobe Brackets" is distributed in the hope that it will be
+useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General
 Public License for more details.
 
-You should have received a copy of the GNU Affero General Public License along 
-with "More Languages for Adobe Brackets". If not, see 
+You should have received a copy of the GNU Affero General Public License along
+with "More Languages for Adobe Brackets". If not, see
 <https://www.gnu.org/licenses/>.
 
 */
 
 define(function (require, exports, module) {
-    
+
     "use strict";
-    
+
     var LanguageManager = brackets.getModule("language/LanguageManager");
-    
+
     // Batch highlighting support.
     //
     //  - Third-party dependency sourced from {CodeStore} Public.
@@ -70,14 +70,14 @@ define(function (require, exports, module) {
         mode:           ["batch", "text/x-batch"],
         fileExtensions: ["bat", "cmd"]
     });
-    
+
     // Brainfuck highlighting support.
     LanguageManager.defineLanguage("brainfuck", {
         name:           "Brainfuck",
         mode:           "brainfuck",
         fileExtensions: ["bf"],
     });
-    
+
     // COBOL highlighting support.
     LanguageManager.defineLanguage("cobol", {
         name:           "COBOL",
@@ -85,7 +85,7 @@ define(function (require, exports, module) {
         fileExtensions: ["cbl", "cob", "cpy"],
         lineComment:    ["*"]
     });
-    
+
     // Common Lisp highlighting support.
     LanguageManager.defineLanguage("commonlisp", {
         name:           "Common Lisp",
@@ -93,7 +93,7 @@ define(function (require, exports, module) {
         fileExtensions: ["cl", "lisp"],
         lineComment:    [";;"]
     });
-    
+
     // Cython highlighting support.
     LanguageManager.defineLanguage("cython", {
         name:           "Cython",
@@ -102,7 +102,7 @@ define(function (require, exports, module) {
         lineComment:    ["#"],
         blockComment:   ["\"\"\"", "\"\"\""]
     });
-    
+
     // D highlighting support.
     LanguageManager.defineLanguage("d", {
         name:           "D",
@@ -111,34 +111,25 @@ define(function (require, exports, module) {
         lineComment:    ["//"],
         blockComment:   ["/*", "*/"]
     });
-	
-    // Django highlighting support.
-    LanguageManager.getLanguage("html").removeFileExtension(["htm", "html"]);
-    
-    LanguageManager.defineLanguage("django", {
-        name:           "Django",
-        mode:           "django",
-        fileExtensions: ["dj.html", "djt", "djt.html", "htm", "html", "html.dj", "html.djt"]
-    });
-    
+
     // Fortran highlighting support.
 	LanguageManager.defineLanguage("fortran", {
         name:           "Fortran",
         mode:           "fortran",
-        fileExtensions: ["F",   "F03", "F08", "F90", 
-                         "F95", "FOR", "FPP", "FTN", 
-                         "f",   "f03", "f08", "f90", 
+        fileExtensions: ["F",   "F03", "F08", "F90",
+                         "F95", "FOR", "FPP", "FTN",
+                         "f",   "f03", "f08", "f90",
                          "f95", "for", "fpp", "ftn"],
         lineComment:    ["!"],
     });
-    
+
     // HTTP highlighting support.
 	LanguageManager.defineLanguage("http", {
         name:           "HTTP",
         mode:           "http",
         fileExtensions: ["http", "https"]
     });
-    
+
     // IDL highlighting support.
 	LanguageManager.defineLanguage("idl", {
         name:           "IDL",
@@ -146,7 +137,7 @@ define(function (require, exports, module) {
         fileExtensions: ["idl"],
         lineComment:    [";;"]
     });
-    
+
     // Julia highlighting support.
 	LanguageManager.defineLanguage("julia", {
         name:           "Julia",
@@ -155,7 +146,7 @@ define(function (require, exports, module) {
         lineComment:    ["#"],
         blockComment:   ["#=", "=#"]
     });
-    
+
     // LiveScript highlighting support.
 	LanguageManager.defineLanguage("livescript", {
         name:           "LiveScript",
@@ -164,7 +155,7 @@ define(function (require, exports, module) {
         lineComment:    ["#"],
         blockComment:   ["/*", "*/"]
     });
-    
+
     // Mathematica highlighting support.
 	LanguageManager.defineLanguage("mathematica", {
         name:           "Mathematica",
@@ -172,7 +163,7 @@ define(function (require, exports, module) {
         fileExtensions: ["m", "mt", "nb"],
         blockComment:   ["(*", "*)"]
     });
-    
+
     // Pascal highlighting support.
 	LanguageManager.defineLanguage("pascal", {
         name:           "Pascal",
@@ -181,7 +172,7 @@ define(function (require, exports, module) {
         lineComment:    ["//"],
         blockComment:   ["(*", "*)"]
     });
-    
+
     // PowerShell highlighting support.
 	LanguageManager.defineLanguage("powershell", {
         name:           "PowerShell",
@@ -190,7 +181,7 @@ define(function (require, exports, module) {
         lineComment:    ["#"],
         blockComment:   ["<#", "#>"]
     });
-	
+
     // R highlighting support.
 	LanguageManager.defineLanguage("r", {
         name:           "R",
@@ -198,14 +189,26 @@ define(function (require, exports, module) {
         fileExtensions: ["R", "RData", "r", "rda", "rds", "Rproj"],
         lineComment:    ["#"]
     });
-    
+
     // reStructuredText (RST / reST) highlighting support.
 	LanguageManager.defineLanguage("restructuredtext", {
         name:           "reStructuredText",
         mode:           "rst",
         fileExtensions: ["rst", "rest", "reST", "REST"]
     });
-    
+
+    LanguageManager.defineLanguage("typescript", {
+      name: 'TypeScript',
+      mode: ['javascript', 'text/typescript'],
+      fileExtensions: ['ts']
+    });
+
+    LanguageManager.defineLanguage("tsx", {
+      name: 'TypeScript-JSX',
+      mode: ['jsx', 'text/typescript-jsx'],
+      fileExtensions: ['tsx']
+    });
+
     // Solidity highlighting support.
     //
     //  - Third-party dependency sourced from GitHub:
@@ -227,14 +230,14 @@ define(function (require, exports, module) {
         mode:           ["solidity", "text/x-solidity"],
         fileExtensions: ["sol"]
     });
-    
+
     // sTeX / LaTeX highlighting support.
 	LanguageManager.defineLanguage("stex", {
         name:           "sTeX",
         mode:           "stex",
         fileExtensions: ["tex", "stex"]
     });
-    
+
     // Swift highlighting support.
 	LanguageManager.defineLanguage("swift", {
         name:           "Swift",
